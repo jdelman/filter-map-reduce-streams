5778420e4f88e4f6842e833f61c27c440e7982db
Josh Delman <joshdelman@gmail.com>
2017-07-26 14:37:34 -0400

    remove isAsync from function signature

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Transformer = require('./transformer');
 
-function filterMapReduceStream(stream, filterFunc, mapFunc, reduceFunc, accumulator, isAsync = false) {
+function filterMapReduceStream(stream, filterFunc, mapFunc, reduceFunc, accumulator) {
   const filterTransformer = new Transformer('filter', filterFunc, null, isAsync);
   const mapTransformer = new Transformer('map', mapFunc, null, isAsync);
   const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator, isAsync);
@@ -8,7 +8,7 @@ function filterMapReduceStream(stream, filterFunc, mapFunc, reduceFunc, accumula
   return stream.pipe(filterTransformer).pipe(mapTransformer).pipe(reduceTransformer);
 }
 
-function reduceStream(stream, reduceFunc, accumulator, isAsync = false) {
+function reduceStream(stream, reduceFunc, accumulator) {
   const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator, isAsync);
   return stream.pipe(reduceTransformer);
 }
