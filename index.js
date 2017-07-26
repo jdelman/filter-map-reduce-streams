cc640155f0cb67c5ee45a80a567f1c816acc2933
Josh Delman <joshdelman@gmail.com>
2017-07-26 15:11:38 -0400

    remove isAsync from helper methods

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Transformer = require('./transformer');
 
 function filterMapReduceStream(stream, filterFunc, mapFunc, reduceFunc, accumulator) {
-  const filterTransformer = new Transformer('filter', filterFunc, null, isAsync);
-  const mapTransformer = new Transformer('map', mapFunc, null, isAsync);
-  const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator, isAsync);
+  const filterTransformer = new Transformer('filter', filterFunc, null);
+  const mapTransformer = new Transformer('map', mapFunc, null);
+  const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator);
 
   return stream.pipe(filterTransformer).pipe(mapTransformer).pipe(reduceTransformer);
 }
 
 function reduceStream(stream, reduceFunc, accumulator) {
-  const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator, isAsync);
+  const reduceTransformer = new Transformer('reduce', reduceFunc, accumulator);
   return stream.pipe(reduceTransformer);
 }
 
